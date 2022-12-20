fda85aa19f42ed5273434f6a07295646894425c6
critx1 <ahmedalawi01@gmail.com>
2022-12-21 00:38:04 +0300

    understanding the project

---
+++ serverProject.cpp
@@ -0,0 +1,32 @@
+/*
+Ahmed Esmail    21906571
+
+Today 20/11/2022 I have started understanding the project, it is simple, there is client and server functions,
+client has has 6 commands, 1- User, 2- LIST, 3- RET, 4-DEL, 5- SEND, 6- QUIT
+each with different arguments, but the user will not use them, instead they will use a menue interface with numbers.
+
+
+*/
+
+
+#include <iostream>
+#include <iostream>
+#include<string>
+#include<string.h>
+#include<iostream>
+#include<stdio.h>
+#include<fstream>
+#include<cstdio>
+#include<sstream>
+using namespace std;
+
+
+int main ()
+{
+
+
+
+
+
+return 0;
+}
